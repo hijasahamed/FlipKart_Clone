11fea60849ef71d7750665b6b602bc25c69af62b
hijas <hijasahamed2000@gmail.com>
2024-03-26 09:47:07 +0530

    latest

--- lib/api_service/api_service.dart
+++ lib/api_service/api_service.dart
@@ -8,43 +8,16 @@ import 'package:flutter_bloc_tutorial_app/screens/home/ui/list_view_electromania
 import 'package:flutter_bloc_tutorial_app/screens/home/models/api_model.dart';
 
 Future<List<ApiModel>> getAllCosmeticProducts() async {
-  const url = 'https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline';
-
-  Uri uri = Uri.parse(url);
-  final respose = await http.get(uri, headers: {'ContentType': 'application/json'});
-  final json = jsonDecode(respose.body) as List;
-
-  final result = json.map((e) => ApiModel.fromjson(e)).toList();
-
-  return result;
-}
-
-Future<List<FakeStoreApiModel>> fetchFakeStoreApiData()async{
-  List<FakeStoreApiModel> res=[];
-  try{
-    const url='https://fakestoreapi.com/products';
-  Uri uri=Uri.parse(url);
-  final response=await http.get(uri);
-  final json=jsonDecode(response.body)as List;
-   res=json.map((e) => FakeStoreApiModel.fromjson(e)).toList();
-  
-  } catch (e){
-    if(kDebugMode){
-      print(e);
-    }
-  }
-  return res;
-}
-
-Future <List<ElectroManiacModel>> electroManiaDataFetch() async {
-  List<ElectroManiacModel> res=[];
+  List<ApiModel> res = [];
   try {
-    const url = 'https://electromania.tech/products';
-    final uri = Uri.parse(url);
-    final respose = await http.get(uri);
-    final item = jsonDecode(respose.body) as List;
-      res=item.map((e) => ElectroManiacModel.fromjson(e)).toList();
-      apiImageAccess(item);
+    const url =
+        'https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline';
+    Uri uri = Uri.parse(url);
+    final respose =
+        await http.get(uri, headers: {'ContentType': 'application/json'});
+    final json = jsonDecode(respose.body) as List;
+
+    res = json.map((e) => ApiModel.fromjson(e)).toList();
   } catch (e) {
     if (kDebugMode) {
       print(e);
@@ -53,17 +26,49 @@ Future <List<ElectroManiacModel>> electroManiaDataFetch() async {
   return res;
 }
 
+Future<List<FakeStoreApiModel>> fetchFakeStoreApiData() async {
+  List<FakeStoreApiModel> res = [];
+  try {
+    const url = 'https://fakestoreapi.com/products';
+    Uri uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final json = jsonDecode(response.body) as List;
+    res = json.map((e) => FakeStoreApiModel.fromjson(e)).toList();
+  } catch (e) {
+    if (kDebugMode) {
+      print(e);
+    }
+  }
+  return res;
+}
 
-Future<List<AllProductCategoryApiModel>> allProductApiDataFetching()async{
-  List<AllProductCategoryApiModel> res=[];
-  try{
-    const url='https://api.escuelajs.co/api/v1/categories';
-  Uri uri=Uri.parse(url);
-  final response=await http.get(uri);
-  final json=jsonDecode(response.body)as List;
-   res=json.map((e) => AllProductCategoryApiModel.fromjson(e)).toList();
-  } catch (e){
-    if(kDebugMode){
+Future<List<ElectroManiacModel>> electroManiaDataFetch() async {
+  List<ElectroManiacModel> res = [];
+  try {
+    const url = 'https://electromania.tech/products';
+    final uri = Uri.parse(url);
+    final respose = await http.get(uri);
+    final item = jsonDecode(respose.body) as List;
+    res = item.map((e) => ElectroManiacModel.fromjson(e)).toList();
+    apiImageAccess(item);
+  } catch (e) {
+    if (kDebugMode) {
+      print(e);
+    }
+  }
+  return res;
+}
+
+Future<List<AllProductCategoryApiModel>> allProductApiDataFetching() async {
+  List<AllProductCategoryApiModel> res = [];
+  try {
+    const url = 'https://api.escuelajs.co/api/v1/categories';
+    Uri uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final json = jsonDecode(response.body) as List;
+    res = json.map((e) => AllProductCategoryApiModel.fromjson(e)).toList();
+  } catch (e) {
+    if (kDebugMode) {
       print(e);
     }
   }
